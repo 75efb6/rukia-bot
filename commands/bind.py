e3e986fa6e313d06f198b37241877719f492545a
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-01-07 21:53:44 +0000

    :art: Format Python code with psf/black

--- commands/bind.py
+++ commands/bind.py
@@ -5,30 +5,43 @@ import aiohttp
 import config
 import hashlib
 
+
 def get_md5_hash(input_string):
     ## Compute MD5 hash
-    return hashlib.md5(input_string.encode("utf-8")).hexdigest() # nosec
+    return hashlib.md5(input_string.encode("utf-8")).hexdigest()  # nosec
+
 
 class Bind(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="bind", description="Bind your profile to your Discord account")
-    async def _bind(self, interaction: nextcord.Interaction, username:str = nextcord.SlashOption(required=True, description="Your username used to log into the game."), password:str = nextcord.SlashOption(required=True, description="Password used to log into the game.")):
+    @nextcord.slash_command(
+        name="bind", description="Bind your profile to your Discord account"
+    )
+    async def _bind(
+        self,
+        interaction: nextcord.Interaction,
+        username: str = nextcord.SlashOption(
+            required=True, description="Your username used to log into the game."
+        ),
+        password: str = nextcord.SlashOption(
+            required=True, description="Password used to log into the game."
+        ),
+    ):
         await interaction.response.defer(ephemeral=True)
         user_id = str(interaction.user.id)
         d_uname = interaction.user.display_name
 
         async with aiohttp.ClientSession() as session:
             api_url = f"{config.domain}/api/login.php"
-            gameversion = 3 ## Game version specified in game server api
-            salted_pswd = password + "taikotaiko" ## Adding salt to password
-            pswd_hash = get_md5_hash(salted_pswd) ## Hashing password
+            gameversion = 3  ## Game version specified in game server api
+            salted_pswd = password + "taikotaiko"  ## Adding salt to password
+            pswd_hash = get_md5_hash(salted_pswd)  ## Hashing password
             username = username.strip()
             params = {
                 "username": username,
                 "password": pswd_hash,
-                "version": str(gameversion)
+                "version": str(gameversion),
             }
             ## Requesting to API for verification process
             async with session.post(api_url, data=params) as response:
@@ -39,27 +52,39 @@ class Bind(commands.Cog):
                         data = response[1].split()
                         if len(data) < 6:
                             print(response + " Failed 2")
-                            await interaction.followup.send("Invalid data sent from server.", ephemeral=True)
+                            await interaction.followup.send(
+                                "Invalid data sent from server.", ephemeral=True
+                            )
                             return
                         ## Parsing User ID from API response
                         uid = int(data[0])
                         ## Check if the uid is already bound to another user
                         existing_user = mongodb_handler.find_user_by_uid(uid)
                         if existing_user and existing_user["_id"] != user_id:
-                            await interaction.followup.send("This UID is already bound to another account.", ephemeral=True)
+                            await interaction.followup.send(
+                                "This UID is already bound to another account.",
+                                ephemeral=True,
+                            )
                             return
                         already_bound = mongodb_handler.get_profile(user_id)
                         if already_bound and already_bound["_id"] == user_id:
-                            await interaction.followup.send("You can't rebind your account, tell Owner for unbind.", ephemeral=True)
+                            await interaction.followup.send(
+                                "You can't rebind your account, tell Owner for unbind.",
+                                ephemeral=True,
+                            )
                             return
                         mongodb_handler.bind_profile(user_id, uid, d_uname, username)
-                        await interaction.followup.send(f"User account {username} has been bound successfully!", ephemeral=True)
+                        await interaction.followup.send(
+                            f"User account {username} has been bound successfully!",
+                            ephemeral=True,
+                        )
                     else:
                         print(response + " Failed 1")
-                        await interaction.followup.send("Invalid data sent from server.", ephemeral=True)
+                        await interaction.followup.send(
+                            "Invalid data sent from server.", ephemeral=True
+                        )
                         return
 
-        
 
 def setup(bot):
     if bot.get_cog("Bind") is None:
--- commands/whitelist_rmdiff.py
+++ commands/whitelist_rmdiff.py
@@ -3,34 +3,52 @@ from nextcord.ext import commands
 import aiohttp
 import config
 
+
 class WhitelistRemoveDiff(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="whitelist_rmdiff", description="Command to remove difficulties of a certain beatmap from the whitelist.", guild_ids=[config.guild_id])
-    async def _whitelist_rmdiff(self, interaction: nextcord.Interaction, diffid:int = nextcord.SlashOption(description="Numerical ID of the difficulty you want to remove from WL.")):
+    @nextcord.slash_command(
+        name="whitelist_rmdiff",
+        description="Command to remove difficulties of a certain beatmap from the whitelist.",
+        guild_ids=[config.guild_id],
+    )
+    async def _whitelist_rmdiff(
+        self,
+        interaction: nextcord.Interaction,
+        diffid: int = nextcord.SlashOption(
+            description="Numerical ID of the difficulty you want to remove from WL."
+        ),
+    ):
         ## Checking if role exists and user has role
         role_id = config.wl_roleid
         role = interaction.guild.get_role(role_id)
 
         if role is None:
-            await interaction.response.send_message("The specified role does not exist in this server.", ephemeral=True)
+            await interaction.response.send_message(
+                "The specified role does not exist in this server.", ephemeral=True
+            )
             return
 
         if role not in interaction.user.roles:
-            await interaction.response.send_message("You do not have the required role to use this command.", ephemeral=True)
+            await interaction.response.send_message(
+                "You do not have the required role to use this command.", ephemeral=True
+            )
             return
-        
+
         await interaction.response.defer()
 
         async with aiohttp.ClientSession() as session:
             ## Calls the private server API
-            api_url = f'{config.domain}/api/wl_remove?key={config.wl_key}&bid={diffid}'
+            api_url = f"{config.domain}/api/wl_remove?key={config.wl_key}&bid={diffid}"
             async with session.get(api_url) as response:
                 if response.status == 200:
                     interaction.followup.send("Done.")
                 else:
-                    await interaction.followup.send("Couldn't remove difficulty from whitelist.")
+                    await interaction.followup.send(
+                        "Couldn't remove difficulty from whitelist."
+                    )
+
 
 def setup(bot):
     if bot.get_cog("WhitelistRemoveDiff") is None:
