49cda7f3ad7eee38390bc0b187b0fb1f6eb4263a
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-03-20 18:52:09 +0100

    Update handlers/apirequests.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- handlers/apirequests.py
+++ handlers/apirequests.py
@@ -91,7 +91,8 @@ class DroidAPI:
                     ## Inserting maps into whitelist
                     api_url = (
                         f"/wl_add?key={self.wl_key}&bid={beatmap_id}"
-                        if isAdd is True else f"/wl_rm?key={self.wl_key}&bid={beatmap_id}"
+                        if isAdd is True
+                        else f"/wl_rm?key={self.wl_key}&bid={beatmap_id}"
                     )
                     response = r().do(
                         isDroid=True, request_type="GET", api_endpoint=api_url
